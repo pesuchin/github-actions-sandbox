600740807ad3d71bbae53cc1e22ee1241625ccb6
pesuchin <pesuchin@users.noreply.github.com>
2021-03-30 05:47:41 +0000

    Apply Code Formatter Change

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,11 +1,10 @@
 def test_hello():
     assert True
 
+
 def sample():
-    return { index: None
-        for index in range(100)
-    }
+    return {index: None for index in range(100)}
+
 
 def yahoo():
-    return [index
-    for index in range(100)]
+    return [index for index in range(100)]
